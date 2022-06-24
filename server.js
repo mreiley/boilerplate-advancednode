7cdead6799bed41403039fa7779265d20a1ce387
Mario Reiley <95431039+mreiley@users.noreply.github.com>
2022-06-24 12:03:09 +0000

    leccion 3:Set up Passport
    se anadio passport y Express-session. para comenzar a usarlas

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+// const path = require("path");
 
 const app = express();
 
@@ -10,9 +11,16 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+// app.set('views', './views/pug')
 
+// app.set("views", path.join(__dirname, "views"));
+
+const ruta = process.cwd() + '/views/pug/';
+// app.route('/').get((req, res) => { // Original
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  // res.render('Load your view here'); // original
+  res.render(ruta +'index',{title: 'Hello', message: 'Please login'});
 });
 
 const PORT = process.env.PORT || 3000;
